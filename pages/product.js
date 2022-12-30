673deba4373974532c35c955861757a982b9d6e8
Ahmed Mohammed <AhmedIsmail73990@azhar.edu.eg>
2022-12-30 17:23:50 +0200

    Update Product Page UI

--- pages/product.js
+++ pages/product.js
@@ -49,7 +49,10 @@ function Product() {
         <>
             <Head>
                 <title>{DUMMY.AssetName}</title>
-                <meta name={'description'} content={`This Page is allow ing you to check and update the product ${DUMMY.AssetName}`} />
+                <meta
+                    name={"description"}
+                    content={`This Page is allow ing you to check and update the product ${DUMMY.AssetName}`}
+                />
             </Head>
             <div className={classes.Product}>
                 <div className={classes.Content}>
@@ -102,83 +105,21 @@ function Product() {
                     </section>
                     <section className={classes.Section_2}>
                         <article className={classes.Admin_Item}>
-                            <h2>Asset Number</h2>
-                            <p>{DUMMY.AssetNumber}</p>
-                        </article>
-                        <article className={classes.Admin_Item}>
                             <h2>Asset Name</h2>
                             <p>{DUMMY.AssetName}</p>
                         </article>
                         <article className={classes.Admin_Item}>
-                            <h2>Tag Number</h2>
-                            <p>{DUMMY.TagNumber}</p>
-                        </article>
-                        <article className={classes.Admin_Item}>
-                            <h2>Asset Category</h2>
-                            <p>{DUMMY.AssetCategory}</p>
-                        </article>
-                        <article className={classes.Admin_Item}>
-                            <h2>Serial</h2>
-                            <p>{DUMMY.Serial}</p>
-                        </article>
-                        <article className={classes.Admin_Item}>
-                            <h2>Plate Number</h2>
-                            <p>{DUMMY.PlateNumber}</p>
+                            <h2>Asset Price</h2>
+                            <p>{DUMMY.AssetNumber}</p>
                         </article>
                     </section>
                     <section className={classes.Section_3}>
                         <article className={classes.User_Item}>
-                            <label htmlFor='location'>Location</label>
+                            <label htmlFor='quantity'>Quantity</label>
                             <input
-                                id='location'
+                                id='quantity'
                                 type={"text"}
-                                placeholder={"Enter Location"}
-                            />
-                        </article>
-                        <article className={classes.User_Item}>
-                            <label htmlFor='inventory'>Inventory</label>
-                            <input
-                                id='inventory'
-                                type={"text"}
-                                placeholder={"Enter Inventory"}
-                            />
-                        </article>
-                        <article className={classes.User_Item}>
-                            <label htmlFor='employee'>Assign to employee</label>
-                            <input
-                                id='employee'
-                                type={"text"}
-                                placeholder={"Enter Employee"}
-                            />
-                        </article>
-                        <article className={classes.User_Item}>
-                            <label htmlFor='AssignFromDate'>
-                                Assign from Date
-                            </label>
-                            <input
-                                id='AssignFromDate'
-                                type={"text"}
-                                placeholder={"Enter Assign from Date"}
-                            />
-                        </article>
-                        <article className={classes.User_Item}>
-                            <label htmlFor='LastTransactionDate'>
-                                Last transaction Date
-                            </label>
-                            <input
-                                id='LastTransactionDate'
-                                type={"text"}
-                                placeholder={"Enter Last transaction Date"}
-                            />
-                        </article>
-                        <article className={classes.User_Item}>
-                            <label htmlFor='Quantity'>
-                                Transaction Quantity
-                            </label>
-                            <input
-                                id='Quantity'
-                                type={"text"}
-                                placeholder={"Enter Transaction Quantity"}
+                                placeholder={"Enter Quantity"}
                             />
                         </article>
                         <div className={classes.BTN_Container}>
